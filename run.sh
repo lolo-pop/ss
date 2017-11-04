a7bc3e1292cb903b9409e9465a2854c8d6c951ba
Rick Yu <cosmtrek@gmail.com>
2017-11-04 15:25:00 +0800

    Fix UDP port

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 chmod +x docker-entrypoint.sh
 docker build -t cosmtrek/ss .
-docker run -d --restart=always --env-file .env -p <PORT>:9000 cosmtrek/ss
+docker run -d --restart=always --env-file .env -p <PORT>:9000 -p <PORT>:9000/udp cosmtrek/ss
